a60cd1093928b21760e6acc999556b23ee84407c
516satyam <516satyam516@gmail.com>
2026-07-02 18:55:53 +0530

    added user and video models

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,27 @@
-const asyncHandler = (fn) => async (req,res,next) => {
-    try{
-        await fn(req, res, next)
-    } catch (error){
-        res.status(err.code || 500).json({
-            success: false,
-            message: err.message
-        })
+// const asyncHandler = () => {}
+// const asyncHandler = () => () => {}
+// const asyncHandler = (func) => async () => {}
+
+// this is the logic of writing the below code
+
+
+// const asyncHandler = (fn) => async (req,res,next) => {
+//     try{
+//         await fn(req, res, next)
+//     } catch (error){
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
+
+//+++++++++ we can do this by promise also, this is the industry standard
+
+const asyncHandler = (requestHandler) =>{
+    return (req, res, next) =>{
+        Promise.resolve(requestHandler(req,res,next)).catch((error) => next(error))
     }
 }
+
+export {asyncHandler}
