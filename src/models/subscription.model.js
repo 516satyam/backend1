174e2cd518456efebe74fcc16c855fd8152a2eff
516satyam <516satyam516@gmail.com>
2026-07-25 16:51:13 +0530

    updated subscription model

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,11 +1,17 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, {Schema, SchemaType} from "mongoose";
 
 const subscriptionSchema = new Schema({
     subscriber:{
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
+        // one who is subscribing
+    },
+    channel: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+        // one to whom subscriber is subscribing
     }
-})
+},{timestamps: true})
 
 
 
